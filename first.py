6a3a4d86094db5f70ec5669c57fb7f730a7684e1
oliver <oliverbinns4221@outlook.com>
2022-09-06 11:37:50 +0100

    added a read me file and 3 text documents

---
+++ first.py
@@ -0,0 +1,12 @@
+name = input('what is your name?')
+age = int(input(f'how old are you {name}?'))
+
+print()
+print(f"hello, {name} and you are {age} correct?") # an f is needed if you want the interpreter to do something to the string before printing such as taking an input and then printing it in a string
+print()
+print(f"you will be {age+1} in a year right?")
+
+
+
+# the bit of code {name} calls the variable which is an input and so it takes an input from the user 
+
---
+++ second.py
@@ -0,0 +1,7 @@
+name = input("what is your name?")
+age = int(input("how old are you?"))
+
+print("")
+print(f"hello, what is your name?")
+print(f"hi {name}! how old are you?")
+print(f"if you are {age} will you be {age+1} in a year right?")
